5e94b17e68f9a16cd51ad5d295bae77ff0fe79b9
IlCarretto <carlorebuffat412@gmail.com>
2022-10-20 15:49:59 +0200

    done with bonus (also button bonus)

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@
 // BONUS 1:
 // [*] Trasformare la stringa foto in una immagine effettiva
 // BONUS 2:
-// Organizzare i singoli membri in card/schede. Se non vi sentite particolarmente creativi, potete prendere uno spunto dallo screenshot allegato.
+// [*] Organizzare i singoli membri in card/schede. Se non vi sentite particolarmente creativi, potete prendere uno spunto dallo screenshot allegato.
 
 
 // MILESTONE 0:
@@ -55,22 +55,61 @@ console.log(teamArray);
 
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
-const row = document.getElementById("ms-row");
+const row = document.querySelector(".row");
 for (let i = 0; i < teamArray.length; i++) {
     const teamEmployee = teamArray[i];
     console.log(teamEmployee);
     // for (let key in teamEmployee) {
     //     console.log(teamEmployee[key]);
     row.innerHTML += `
+    <div class="col-4">
         <div class="ms-card">
-            <h1 id="name">
-            ${teamEmployee.nomeCompleto}
-            </h1>
-            <h2 id="role">
-            ${teamEmployee.ruolo}
-            </h2>
-            <img id="photo" src="
-            ${teamEmployee.foto}" alt="">
+            <div class="card-top-content">
+                <img id="photo" src="
+                ${teamEmployee.foto}" alt="">
+            </div>
+            <div class="card-bottom-content">
+                <h1 id="name">
+                ${teamEmployee.nomeCompleto}
+                </h1>
+                <h2 id="role">
+                ${teamEmployee.ruolo}
+                </h2>
+            </div>
         </div>
+    </div>
     `
 };
+
+// [] BONUS 3: Inserire un bottone per nuova card con un altro membro 
+// Al click del bottone appaiono 3 prompt per l'utente per le info
+// Pushare le nuove info 
+const insertBtn = document.querySelector(".insert-btn");
+insertBtn.addEventListener("click", function() {
+    const userNewName = prompt("Inserisci il nome del nuovo membro");
+    const userNewRole = prompt("Inserisci il ruolo del nuovo membro");
+    const userNewPhoto = prompt("Inserisci la foto del nuovo membro");
+    const newEmployee = {
+        nomeCompleto: userNewName,
+        ruolo: userNewRole,
+        foto: userNewPhoto
+    };
+    row.innerHTML += `
+    <div class="col-4">
+        <div class="ms-card">
+            <div class="card-top-content">
+                <img id="photo" src="
+                ${newEmployee.foto}" alt="">
+            </div>
+            <div class="card-bottom-content">
+                <h1 id="name">
+                ${newEmployee.nomeCompleto}
+                </h1>
+                <h2 id="role">
+                ${newEmployee.ruolo}
+                </h2>
+            </div>
+        </div>
+    </div>
+    `
+})
